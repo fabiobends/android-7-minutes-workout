637f72e6cdb28c275ee1c7780530328ab4ec3238
Fábio Mendes <fabiobends@gmail.com>
2022-12-30 21:17:17 -0300

    feat: add `ExerciseActivity`
    Add `ExerciseActivity` and its custom toolbar.

--- app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.a7minutesworkout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
@@ -15,7 +16,8 @@ class MainActivity : AppCompatActivity() {
     setContentView(binding?.root)
 
     binding?.startButton?.setOnClickListener {
-      Toast.makeText(this, "Pressed", Toast.LENGTH_LONG).show()
+      val intent = Intent(this, ExerciseActivity::class.java)
+      startActivity(intent)
     }
   }
 
