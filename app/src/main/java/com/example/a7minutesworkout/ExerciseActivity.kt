b33c17c9e640648e15b41aa126e565124a37413d
Fábio Mendes <fabiobends@gmail.com>
2023-01-04 21:28:07 -0300

    feat: add countdown timer
    Add countdown timer when tapping the start button.

--- app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
@@ -2,10 +2,15 @@ package com.example.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import com.example.a7minutesworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
   private var binding: ActivityExerciseBinding? = null
+  private var timer: CountDownTimer? = null
+  private var restProgress = 0
+  private var startTime: Long = 10000
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -16,5 +21,43 @@ class ExerciseActivity : AppCompatActivity() {
     binding?.toolbarExercise?.setNavigationOnClickListener {
       onBackPressed()
     }
+    setRestView()
+  }
+
+  private fun setRestView(){
+    timer?.let {
+      it.cancel()
+      restProgress = 0
+    }
+    setRestProgressBar()
+  }
+
+
+  private fun setRestProgressBar() {
+    binding?.progressBar?.progress = restProgress
+    timer = object : CountDownTimer(startTime, 1000) {
+      override fun onTick(millisUntilFinished: Long) {
+        restProgress++
+        val progress = (startTime/1000).toInt() - restProgress
+        binding?.progressBar?.progress = progress
+        binding?.timer?.text = progress.toString()
+      }
+      override fun onFinish() {
+        Toast.makeText(
+          this@ExerciseActivity,
+          "Here now we will start the exercise",
+          Toast.LENGTH_LONG
+        ).show()
+      }
+    }.start()
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    timer?.let {
+      it.cancel()
+      restProgress = 0
+    }
+    binding = null
   }
 }
